5e54c9314adf9b80e918a1f661600be9d787c5a8
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-23 22:41:43 +0200

    feat(creazione dei i due fail detail e home  e la configuazione dentro app.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-import Header from './components/Header';
+import DefaultLayout from './layout/DefaultLayout'
+import DetailFilm from './modules/pages/DetailFilm'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import HomePages from './modules/pages/HomePages'
 
 const App = () => {
-  return (
-    <>
-    <Header/>
-      
-    </>
-  )
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<HomePages />} />
+          <Route path="/books/:id" element={<DetailFilm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+ 
+)
 }
 
 export default App
---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import {Outlet} from  'react-router-dom'
+const DefaultLayout = () => {
+  return (
+    <>
+    <Header/>
+    <main>
+        <Outlet/>
+    </main>
+
+      
+    </>
+  );
+}
+
+export default DefaultLayout;
