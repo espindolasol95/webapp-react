c73928d9dc34448027cc2ae4114856b2769bf421
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-28 11:26:40 +0200

    feat(parte statica di detail film)

--- src/pages/DetailFilm.jsx
+++ src/pages/DetailFilm.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 
 const DetailFilm = () => {
   return (
-    <div>
-      
-    </div>
-  );
+    <div className='container'>  
+     <div className="row">
+      <div className="col-12">
+        <div className="detail card">
+          <div className="d-flex">
+            <div className="cover img">
+              <img src={film.cover} alt={film.title} />
+            </div>
+            <div className='text-details ms-3'>
+              <h2>{film.title}</h2>
+              <p>{film.description}</p>
+              <h4>recensioni</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
+    </div> 
+  )
 }
 
 export default DetailFilm;
