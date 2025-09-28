ad62383feef87e9fb2c69a1178185d28cb637ca4
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-28 11:39:07 +0200

    feat(preso l'id dall url e faccio chiamata api )

--- src/pages/DetailFilm.jsx
+++ src/pages/DetailFilm.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import {useParams} from 'react-router-dom'
+import axios from 'axios';
 
 const DetailFilm = () => {
+  const {id} =useParams ()
+  const [film, setFilm] = useState (null)
+  const [loading, setLoading] = useState (true)
+
+    useEffect(() => {
+    const getFilm = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3001/api/films/${id}`);
+        setFilm(res.data); 
+      } catch (err) {
+        console.error(err);
+        setFilm(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFilm();
+  }, [id]);
+
+  if (loading) return <p>Caricamento...</p>;
+  if (!film) return <p>Film non trovato</p>;
+
+
   return (
     <div className='container'>  
      <div className="row">
